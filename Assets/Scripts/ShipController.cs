031d81aa3e90a736d29e13b36a7eddfb3f82671d
Liam Arbuckle <larbuckle@liams-air.lan>
2021-09-22 19:01:44 +0800

    🚶🏻 Adding scaffolding for turning the ship and adding ship acceleration to the ship controller for UD1-1

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -6,6 +6,14 @@ public class ShipController : MonoBehaviour
 {
     public float forwardSpeed = 25f, strafeSpeed = 7.5f, hoverSpeed = 5f; // forward/backwards movement, sideways movement, up/down movement
     private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
+    private float forwardAcceleration = 2.5f, strafeAcceleration = 2f, hoverAcceleration = 2f;
+
+    // Animations for moving/rotation of ship
+    public float lookRateSpeed = 90f;
+    private Vector2 lookInput, screenCenter, mouseDistance; // Getting the mouse position
+    /*
+    The ship will turn based on where the mouse is moving (how far and what direction it is from the center of the screen. If the mouse is to the right of the screen, the ship will turn right)
+    */
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +24,9 @@ public class ShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        activeForwardSpeed = Input.GetAxisRaw("Vertical") * forwardSpeed;
-        activeStrafeSpeed = Input.GetAxisRaw("Horizontal") * strafeSpeed;
-        activeHoverSpeed = Input.GetAxisRaw("Hover") * hoverSpeed; // Created hover axis in Unity project settings
+        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
+        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
+        activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Hover") * hoverSpeed, hoverAcceleration * Time.deltaTime);
 
         transform.position = transform.forward * activeForwardSpeed * Time.deltaTime; // .forward is whatever direction the ship is currently facing
         transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed) * Time.deltaTime; // Make the camera a child of the ship so the camera follows the ship
