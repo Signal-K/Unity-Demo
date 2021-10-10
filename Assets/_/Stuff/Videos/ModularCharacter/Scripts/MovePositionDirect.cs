a880f4105dc1ddd2a1056ec9dd1f50e0fd17cb02
Gizmotronn <droidology@allianceofdroids.org.au>
2021-10-10 16:27:50 +0800

    Initialising UD1-1 GSCA-9 for https://signal-k.slack.com/files/T02FNAF6Y5V/F02HBFUBVUL

---
+++ Assets/_/Stuff/Videos/ModularCharacter/Scripts/MovePositionDirect.cs
@@ -0,0 +1,35 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePositionDirect : MonoBehaviour, IMovePosition {
+
+    private Vector3 movePosition;
+
+    private void Awake() {
+        movePosition = transform.position;
+    }
+
+    public void SetMovePosition(Vector3 movePosition) {
+        this.movePosition = movePosition;
+    }
+
+    private void Update() {
+        Vector3 moveDir = (movePosition - transform.position).normalized;
+        if (Vector3.Distance(movePosition, transform.position) < 1f) moveDir = Vector3.zero; // Stop moving when near
+        GetComponent<IMoveVelocity>().SetVelocity(moveDir);
+    }
+
+}
---
+++ Assets/_/Stuff/Videos/ShootProjectiles/Scripts/Target.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Target : MonoBehaviour {
+
+    private static List<Target> targetList;
+    
+    public static Target GetClosest(Vector3 position, float maxRange) {
+        Target closest = null;
+
+        Vector3 targetSpriteOffset = new Vector3(0, 7.35f);
+
+        foreach (Target target in targetList) {
+            if (Vector3.Distance(position, target.GetPosition() + targetSpriteOffset) <= maxRange) {
+                if (closest == null) {
+                    closest = target;
+                } else {
+                    if (Vector3.Distance(position, target.GetPosition() + targetSpriteOffset) < Vector3.Distance(position, closest.GetPosition() + targetSpriteOffset)) {
+                        closest = target;
+                    }
+                }
+            }
+        }
+        return closest;
+    }
+
+
+
+    private Animation animation;
+
+    private void Awake() {
+        if (targetList == null) targetList = new List<Target>();
+        targetList.Add(this);
+        animation = transform.Find("Sprite").GetComponent<Animation>();
+    }
+
+    public void Damage() {
+        animation.Play();
+        Instantiate(GameAssets.i.pfSmoke, transform.position + new Vector3(0, 7.35f) + UtilsClass.GetRandomDir() * Random.Range(0f, 2.2f), Quaternion.identity);
+    }
+
+    public Vector3 GetPosition() {
+        return transform.position;
+    }
+
+}
