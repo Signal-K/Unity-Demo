551b06e501571022c29e150c6082c26ae90de12c
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-10-23 15:29:57 +0800

    UD1-1 Applying biome type to texture

--- Assets/PCG/Scripts/BiomeBuilder.cs
+++ Assets/PCG/Scripts/BiomeBuilder.cs
@@ -13,7 +13,28 @@ public class BiomeBuilder : MonoBehaviour
 
     // Build the texture for the biome
     public Texture2D BuildTexture (TerrainType[,] heatMapTypes, TerrainType[,] moistureMapTypes) {
-        return null;
+        int size = heatMapTypes.GetLength(0);
+        Color[] pixels = new Color[size * size];
+
+        for(int x = 0; x < size; x++) {
+            for(int z = 0; z < size; z++) {
+                int index = (x * size) + z;
+                int heatMapIndex = heatMapTypes[x, z].index;
+                int moistureMapIndex = moistureMap[x, z].index;
+
+                Biome biome = biomeRows[moistureMapIndex].biomes[heatMapIndex]; // Pin pointing a specific biome on the biome table based on the heatMapIndex and the moistureMapIndex
+                pixels[index] = biome.color;
+            }
+        }
+
+        // Assign the colour of the biome to a texture
+        Texture2D texture = new Texture2D(size, sizw);
+        texture.wrapMode = TextureWrapMode.Clamp;
+        texture.filterMode = FilterMode.Bilinear;
+        texture.SetPixels(pixels);
+        texture.Apply();
+
+        return texture;
     }
 }
 
