c02bcf6b5ac4f21d887f7b65ba53048a9d9f56b2
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-10-29 13:09:12 +0800

    #3 mesh/triangle percentages

--- Assets/PCG/Planet Procedural Generation/Scripts/TerrainFace.cs
+++ Assets/PCG/Planet Procedural Generation/Scripts/TerrainFace.cs
@@ -22,9 +22,12 @@ public class TerrainFace {
         Vector3[] vertices = new Vector3[resolution * resolution]; // Resolution = number of verices across a single face
         int[] triangles = new int[(resolution-1)*(resolution-1)*2*3];
 
-        for(int y = 0; y = resolution; y++) {
+        for(int y = 0; y < resolution; y++) {
             for(int x = 0; x < resolution; x++) {
-                Vector2 percent = new Vector2(x, y) / (resolution - 1);
+                int i = x + y * resolution; // Number of iterations of the inner loop + outer loop
+                Vector2 percent = new Vector2(x, y) / (resolution - 1); // When x is at highest point, it will be equal to 1% (lowest point 0%) # how close to complete each loop is - where should the vertex be on each face?
+                Vector3 pointOnUnitCube = localUp + (percent.x-.5f)*2*axisA + (percent.y - .5f) * 2 * axisB;// ^How far along the axis we are
+                vertices[i] = pointOnUnitCube;
             }
         }
     }
