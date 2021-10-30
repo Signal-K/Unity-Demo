849a0116c467fac14955a63f227aaa31a952e043
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-10-30 13:24:35 +0800

    #3 UD1-1 GSCA-9 GenerateMesh TerrainFace

--- Assets/PCG/Planet Procedural Generation/Scripts/Planet.cs
+++ Assets/PCG/Planet Procedural Generation/Scripts/Planet.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Range(2,256)]
+    public int resolution = 10;
+    
+    [SerializeField, HideInInspector]
+    MeshFilter[] meshFilters;
+    TerrainFace[] terrainFaces;
+
+    private void OnValidate() {
+        Initialize();
+        GenerateMesh();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Initialize() {
+        if(meshFilters == null || meshFilters.Length == 0) {
+            meshFilters = new MeshFilter[6];
+        }
+        terrainFaces = new TerrainFace[6];
+
+        Vector3[] directions = {Vector3.up,Vector3.down,Vector3.left,Vector3,right,Vector3.forward,Vector3.back};
+
+        for(int i = 0; i < 6; i++) {
+            GameObject meshObj = new GameObject("mesh");
+            meshObj.transform.parent = transform;
+
+            meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+            meshFilters[i] = meshObj.AddComponent<meshFilter>();
+            meshFilters[i].sharedMesh = new Mesh();
+
+            terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+        }
+    }
+
+    void GenerateMesh() {
+        foreach (TerrainFace face in terrainFaces) {
+            face.ConstructMesh();
+        }
     }
 }
--- Assets/PCG/Planet Procedural Generation/Scripts/TerrainFace.cs
+++ Assets/PCG/Planet Procedural Generation/Scripts/TerrainFace.cs
@@ -21,6 +21,7 @@ public class TerrainFace {
     public ConstructMesh() {
         Vector3[] vertices = new Vector3[resolution * resolution]; // Resolution = number of verices across a single face
         int[] triangles = new int[(resolution-1)*(resolution-1)*2*3];
+        int triIndex = 0; // We can create triangles as long as the current vertex is not along the right or bottom edge
 
         for(int y = 0; y < resolution; y++) {
             for(int x = 0; x < resolution; x++) {
@@ -28,7 +29,22 @@ public class TerrainFace {
                 Vector2 percent = new Vector2(x, y) / (resolution - 1); // When x is at highest point, it will be equal to 1% (lowest point 0%) # how close to complete each loop is - where should the vertex be on each face?
                 Vector3 pointOnUnitCube = localUp + (percent.x-.5f)*2*axisA + (percent.y - .5f) * 2 * axisB;// ^How far along the axis we are
                 vertices[i] = pointOnUnitCube;
+
+                // The two triangles of the square in the array
+                if(x != resolution - 1 && y != resolution - 1) {
+                    triangles[triIndex] = i;
+                    triangles[triIndex+1] = i+resolution+1;
+                    triangles[triIndex+2] = i+resolution; // End of first triangle
+
+                    triangles[triIndex+3] = i;
+                    triangles[triIndex+4] = i + 1;
+                    triangles[triIndex+5] = i + resolution+1;
+                    triIndex += 6;
+                }
             }
         }
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
     }
 }
