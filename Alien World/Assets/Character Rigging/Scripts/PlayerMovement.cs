9165101c72b123ecced85331ad7d3bcd40912820
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-11-13 13:50:50 +0800

    🎦🎞📽 Sorting out animations for #3 character rigging

--- Alien World/Assets/Character Rigging/Scripts/PlayerMovement.cs
+++ Alien World/Assets/Character Rigging/Scripts/PlayerMovement.cs
@@ -17,17 +17,25 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private float gravity;
 
+    [SerializeField] private float jumpHeight;
+
     // References
     private CharacterController controller;
+    private Animator anim;
 
     private void Start() {
         // Gets called whenever the game starts
         controller = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>(); // Set the animator to be equal to the player animator (into first child of the "Player" game object)
     }
 
     private void Update() {
         // Called each frame
         Move();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) { // If we press left click
+            StartCoroutine(Attack());
+        }
     }
 
     private void Move() {
@@ -40,21 +48,28 @@ public class PlayerMovement : MonoBehaviour
         float moveZ = Input.GetAxis("Vertical"); // z = forward and backwards input axes
 
         moveDirection = new Vector3(0, 0, moveZ);
-        
-        if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift)) { // if not (0,0,0) and the left shift key is NOT being held down
-            // We are walking
-            Walk();
-        }
-        else if(moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift)) {
-            // We are sprinting/running
-            Run();
-        }
-        else if(moveDirection == Vector3.zero) { // If there is no movement
-            // We are idle/not moving
-            Idle();
-        }
+        moveDirection = transform.TransformDirection(moveDirection); // The forward is tied to the direction the player is facing in
 
-        moveDirection *= moveSpeed;
+        if(isGrounded) { // If the player is on the ground, we can walk/move the player
+            if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift)) { // if not (0,0,0) and the left shift key is NOT being held down
+                // We are walking
+                Walk();
+            }
+            else if(moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift)) {
+                // We are sprinting/running
+                Run();
+            }
+            else if(moveDirection == Vector3.zero) { // If there is no movement
+                // We are idle/not moving
+                Idle();
+            }
+
+            moveDirection *= moveSpeed;
+
+            if(Input.GetKeyDown(KeyCode.Space)) {
+                Jump();
+            }
+        }
 
         controller.Move(moveDirection * Time.deltaTime);
 
@@ -63,14 +78,28 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Idle() {
-        
+        anim.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
     }
 
     private void Walk() {
         moveSpeed = walkSpeed;
+        anim.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
     }
 
     private void Run() {
         moveSpeed = runSpeed;
+        anim.SetFloat("Speed", 1f, 0.1f, Time.deltaTime); // Params -> float, value, dampTime value, T.dT value
+    }
+
+    private void Jump() {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity); // Amount the player will jump (move upwards on the y axis)
+    }
+
+    private IEnumerator Attack() {
+        anim.SetLayerWeight(anim.GetLayerIndex("Attack layer"), 1); // Set the weight of the attack layer to 1
+        anim.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(0.9f);
+        anim.SetLayerWeight(anim.GetLayerIndex("Attack layer"), 0); // Set the weight of the attack layer to 0
     }
 }
