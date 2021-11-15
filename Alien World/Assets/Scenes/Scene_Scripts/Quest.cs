fc2bfc7d51ce23e290c955fbe33bc4630df3ae64
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-11-15 10:43:58 +0800

    🖼  Set up QuestWindow (UI) for #7

---
+++ Alien World/Assets/Scenes/Scene_Scripts/Quest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest
+{
+    public bool isActive; // Is the quest active (if so pop up the quest window ui)
+
+    public string title;
+    public string description;
+    public int experienceReward;
+    public int currencyReward;
+}
