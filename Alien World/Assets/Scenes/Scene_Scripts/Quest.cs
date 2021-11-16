5d17259c60f2bc5aa615425aa17f4d5dd0e1fafb
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-11-16 21:40:06 +0800

    Failing to figure out how to add value to currentAmount for individual quests for #7

--- Alien World/Assets/Scenes/Scene_Scripts/Quest.cs
+++ Alien World/Assets/Scenes/Scene_Scripts/Quest.cs
@@ -13,4 +13,9 @@ public class Quest
     public int currencyReward;
 
     public QuestGoal goal;
+
+    public void Complete() {
+        isActive = false;
+        Debug.Log(title + " was completed");
+    }
 }
--- Alien World/Assets/Scenes/Scene_Scripts/QuestGoal.cs
+++ Alien World/Assets/Scenes/Scene_Scripts/QuestGoal.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 [System.Serializable]
 public class QuestGoal {
+    public Player player;
+
     public GoalType goalType;
 
     public int requiredAmount;
@@ -11,6 +13,31 @@ public class QuestGoal {
 
     public bool IsReached() { // Has the goal been reached?
        return (currentAmount >= requiredAmount);
+    }
+
+    public void MaterialGathered() { 
+        /*
+        For demo purposes:
+        The demo quest is collecting materials, this function is run whenever a material is collected
+        When the currentAmount variable is >= requiredAmount, the quest is finished!
+        
+        This is a demo method to create an update for the currentAmount, however later we'll create a proper scene to show this off
+        */
+
+        if(goalType == GoalType.Gathering) // Only do this if the goal for the quest is to gather materials
+            currentAmount++;
+    }
+
+    public void EnemyKilled() {
+        if(goalType == GoalType.Kill)
+            currentAmount++;
+    }
+
+
+    // Specific quest types scaffolding
+    public void FightEnemy() {
+        player.health -= 1;
+        EnemyKilled();
     }
 }
 
