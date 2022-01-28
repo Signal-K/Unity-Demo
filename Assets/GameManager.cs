af85014a201604aad0b5be821cff6884be5dd828
Gizmotronn <droidology@allianceofdroids.org.au>
2022-01-29 00:00:23 +1100

    🙌🏻🎉 -> Further work on context UI for #5

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,6 +10,9 @@ public class GameManager : MonoBehaviour {
     public GameObject CreatePlayerButton;
     public GameObject LoginButton;
     Guid currentDeviceId;
+    public Slider LevelSlider;
+    public GameObject GiveXPButton;
+    public Text CurrentLevelText, NextLevelText, CurrentXpText;
 
     public void CreatePlayer() {
         currentDeviceId = Guid.NewGuid();
@@ -18,8 +21,16 @@ public class GameManager : MonoBehaviour {
         // Create player
         LootLockerSDKManager.StartSession(currentDeviceId.ToString(), (response) => {
             if(response.success) {
+                CreatePlayerButton.SetActive(false);
+                LoginButton.SetActive(false);
+                PlayerNameInputField.gameObject.SetActive(false);
+
+                GiveXPButton.SetActive(true);
+                LevelSlider.gameObject.SetActive(true);
+                XpIncreaseInputField.gameObject.SetActive(true);
                 LootLockerSDKManager.SetPlayerName(PlayerNameInputField.text, null);
                 Debug.Log("Success");
+                CheckLevel();
             } else {
                 Debug.Log("Failed, " + response.Error);
             }
@@ -29,7 +40,15 @@ public class GameManager : MonoBehaviour {
     public void Login() {
         LootLockerSDKManager.StartSession(PlayerPrefs.GetString("GUID", ""), (response => {
             if (response.success) {
+                CreatePlayerButton.SetActive(false);
+                LoginButton.SetActive(false);
+                PlayerNameInputField.gameObject.SetActive(false);
+
+                GiveXPButton.SetActive(true);
+                LevelSlider.gameObject.SetActive(true);
+                XpIncreaseInputField.gameObject.SetActive(true);
                 Debug.Log("Success");
+                CheckLevel();
             } else {
                 Debug.Log("Failed, " + response.Error);
             }
@@ -43,8 +62,25 @@ public class GameManager : MonoBehaviour {
                 LoginButton.SetActive(false);
                 PlayerNameInputField.gameObject.SetActive(false);
                 Debug.Log("Success");
+                CheckLevel();
             } else {
                 Debug.Log("Failed, " + response.Error);
+            }
+        });
+    }
+
+    public void CheckLevel() {
+        LootLockerSDKManager.GetPlayerInfo((response) => {
+            CurrentLevelText.text = response.level.ToString();
+            NextLevelText.text = (response.level + 1).ToString();
+            CurrentXpText.text = response.xp.ToString() + " / " + response.level_thresholds.next.ToString(); // Displays how much xp we currently have and how much XP is needed to level up
+
+            if(LevelSlider.value == LevelSlider.maxValue) { // Xp threshold for levelling up met
+                LevelSlider.maxValue = response.level_thresholds.next - (float)response.xp - int.Parse(XpIncreaseInputField.text);
+                LevelSlider.value = 0;
+                // This assumes that the threshold/difference between levels will always be 100xp. Once the player gets to the next set of 100, the slider will go back to 0 (the xp total won't). We can change this if we want to have different thresholds by turning the float for xp into a percentage of the total xp needed to reach the next level versus the xp the player has now
+            } else {
+                LevelSlider.value = (float)response.xp - response.level_thresholds.current;
             }
         });
     }
